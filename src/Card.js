002c78c3f5265b86af7a5e6ad81d41997951d301
Alexey Shelyagin <alexeyshelyagin@gmail.com>
2019-03-06 20:20:32 +0300

    cardd

---
+++ src/Card.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import './material.min.css';
+
+var card_BG = {
+  //background: "url(" + {this.props.source} + ") center / cover"
+};
+
+class Card extends Component {
+  render () {
+    return (
+      <div class="card">
+        <div class="demo-card-wide mdl-card mdl-shadow--2dp" >
+          <div class="mdl-card__title" style={card_BG}>
+            <h2 class="mdl-card__title-text">Welcome</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris sagittis pellentesque lacus eleifend lacinia...
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+              Get Started
+            </a>
+          </div>
+          <div class="mdl-card__menu">
+            <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+              <i class="material-icons">+</i>
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Card;
