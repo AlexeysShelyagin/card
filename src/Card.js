971e506d214bccfce63a579ed892ad96d2f70a4b
Alexey Shelyagin <alexeyshelyagin@gmail.com>
2019-03-07 22:18:23 +0300

    Card

--- src/Card.js
+++ src/Card.js
@@ -2,26 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import './material.min.css';
 
-var card_BG = {
-  //background: "url(" + {this.props.source} + ") center / cover"
-};
 
 class Card extends Component {
   render () {
+    var card_BG = {background: "url(" + this.props.source + ") center / cover"};
     return (
       <div class="card">
-        <div class="demo-card-wide mdl-card mdl-shadow--2dp" >
+        <div class="demo-card-wide mdl-card mdl-shadow--2dp">
           <div class="mdl-card__title" style={card_BG}>
-            <h2 class="mdl-card__title-text">Welcome</h2>
+            <h2 class="mdl-card__title-text">{this.props.name}</h2>
           </div>
           <div class="mdl-card__supporting-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Mauris sagittis pellentesque lacus eleifend lacinia...
           </div>
           <div class="mdl-card__actions mdl-card--border">
-            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+            <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
               Get Started
-            </a>
+            </button>
           </div>
           <div class="mdl-card__menu">
             <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css'
 import App from './App';
 import Card from './Card'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />
-  //<Card source = "https://i.imgur.com/665nscC.jpg" />
+  <div >
+    <Card name = "Hello world!" source= "https://i.imgur.com/665nscC.jpg" />
+  </div>
   , document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
